21f642b7bdcb891f4333a749472213323b71c0c0
BotDevilLozan <118263959+BotDevilLozan@users.noreply.github.com>
2023-07-06 17:14:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ module.exports = {
     REJECT_CALLS: convertToBool(process.env.REJECT_CALLS) || true,
     PMB: process.env.PMB || '_Personal messages not allowed, BLOCKED!_',
     READ_COMMAND: convertToBool(process.env.READ_COMMAND) || true,
-    SESSION: (process.env.SESSION || process.env.SESSION_ID || '').trim() || 'Raganork~08357e09c42678658e31dc6713177ba0:444a324b39337634794f343d',
+    SESSION: (process.env.SESSION || process.env.SESSION_ID || '').trim() || 'Raganork~f5a2e52635666b84364a5e74b7f88693:38576d2b58625a656f5a553d',
     IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
     RG: process.env.RG || '919074309534-1632403322@g.us,120363116963909366@g.us',
     BOT_INFO: process.env.BOT_INFO || 'Raganork;Skl11;0;https://i.imgur.com/P7ziVhr.jpeg;https://chat.whatsapp.com/Dt3C4wrQmt0GG6io1IBIHb',
